87281b25ae5f1cdc19bd476ceaebf72098ee389d
Tran Duc Tan <tantd90.ktmt@gmail.com>
2020-04-09 22:17:14 +0700

    Test read db

--- app/src/main/java/com/tantd/spyzie/data/db/AppDbManager.java
+++ app/src/main/java/com/tantd/spyzie/data/db/AppDbManager.java
@@ -52,26 +52,55 @@ public class AppDbManager implements DbManager {
 
     @Override
     public <T> void put(T t) {
-
+        if (t instanceof Location) {
+            mLocationBox.put((Location) t);
+        } else if (t instanceof Sms) {
+            mSmsBox.put((Sms) t);
+        } else if (t instanceof Call) {
+            mCallBox.put((Call) t);
+        } else if (t instanceof Contact) {
+            mContactBox.put((Contact) t);
+        }
     }
 
     @Override
     public <T> void put(T... ts) {
-
+        for (T t : ts) {
+            put(t);
+        }
     }
 
     @Override
     public <T> void put(Collection<T> collection) {
-
+        for (T t : collection) {
+            put(t);
+        }
     }
 
     @Override
     public <T> List<T> findAll(Class<T> tClass) {
+        if (Location.class.equals(tClass)) {
+            return (List<T>) mLocationQuery.find();
+        } else if (Sms.class.equals(tClass)) {
+            return (List<T>) mSmsQuery.find();
+        } else if (Call.class.equals(tClass)) {
+            return (List<T>) mCallQuery.find();
+        } else if (Contact.class.equals(tClass)) {
+            return (List<T>) mContactQuery.find();
+        }
         return null;
     }
 
     @Override
     public <T> void removeAll(Class<T> tClass) {
-
+        if (Location.class.equals(tClass)) {
+            mLocationBox.removeAll();
+        } else if (Sms.class.equals(tClass)) {
+            mSmsBox.removeAll();
+        } else if (Call.class.equals(tClass)) {
+            mCallBox.removeAll();
+        } else if (Contact.class.equals(tClass)) {
+            mContactBox.removeAll();
+        }
     }
 }
