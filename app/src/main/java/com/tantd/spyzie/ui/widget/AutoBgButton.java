eaa5a1e4d067a5d215a712a408f06eabf52fe544
td.tan <td.tan@alticast.com>
2020-06-01 10:21:53 +0700

    Update configuration screen

---
+++ app/src/main/java/com/tantd/spyzie/ui/widget/AutoBgButton.java
@@ -0,0 +1,81 @@
+package com.tantd.spyzie.ui.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class AutoBgButton extends AppCompatButton {
+
+    public AutoBgButton(Context context) {
+        super(context);
+    }
+
+    public AutoBgButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoBgButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setBackground(Drawable d) {
+        AutoBackgroundDrawable layer = new AutoBackgroundDrawable(d);
+        super.setBackground(layer);
+    }
+
+    /**
+     * The stateful LayerDrawable used by this Button.
+     */
+    protected class AutoBackgroundDrawable extends LayerDrawable {
+        // The color filter to apply when the button is pressed
+        protected ColorFilter _pressedFilter = new LightingColorFilter(Color.LTGRAY, 1);
+        // Alpha value when the button is disabled
+        protected int _disabledAlpha = 100;
+        // Alpha value when the button is enabled
+        protected int _fullAlpha = 255;
+
+        public AutoBackgroundDrawable(Drawable d) {
+            super(new Drawable[]{d});
+        }
+
+        @Override
+        protected boolean onStateChange(int[] states) {
+            boolean enabled = false;
+            boolean pressed = false;
+
+            for (int state : states) {
+                if (state == android.R.attr.state_enabled)
+                    enabled = true;
+                else if (state == android.R.attr.state_pressed)
+                    pressed = true;
+            }
+
+            mutate();
+            if (enabled && pressed) {
+                setColorFilter(_pressedFilter);
+            } else if (!enabled) {
+                setColorFilter(null);
+                setAlpha(_disabledAlpha);
+            } else {
+                setColorFilter(null);
+                setAlpha(_fullAlpha);
+            }
+
+            invalidateSelf();
+
+            return super.onStateChange(states);
+        }
+
+        @Override
+        public boolean isStateful() {
+            return true;
+        }
+    }
+}
